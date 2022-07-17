859a605b2c71f0e0828e791acafdb50eda5268b1
ValentynaBondar <105191593+ValentynaBondar@users.noreply.github.com>
2022-07-17 16:38:30 +0000

    Deploying to gh-pages from @ ValentynaBondar/gh-pages-@a19c8360fe7c2b3a40d68435565b1d925e3cef67 🚀

--- index.793bcdd6.js
+++
@@ -1,2 +0,0 @@
-var time=2,cc=1;$(window).scroll((function(){$("#counter").each((function(){$(this).offset().top<$(window).scrollTop()+300&&cc<2&&($(".advantages__item").addClass("viz"),$("h3").each((function(){var t=1,c=$(this).data("num"),n=1e3*time/c,a=$(this),e=setInterval((function(){t<=c?a.html(t):(cc+=2,clearInterval(e)),t++}),n)})))}))}));
-//# sourceMappingURL=index.793bcdd6.js.map
