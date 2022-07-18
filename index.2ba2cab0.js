da254389e64a6e472702817d4afb8868e5f3aa5f
Tetiana-Shev <104375923+Tetiana-Shev@users.noreply.github.com>
2022-07-18 20:43:05 +0000

    Deploying to gh-pages from @ ValentynaBondar/gh-pages-@05db70b3eb1043bf7a81d483e115cf5eeab9bca4 🚀

---
+++ index.2ba2cab0.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-about-open]"),closeModalBtn:document.querySelector("[data-modal-about-close]"),modal:document.querySelector("[data-modal-about]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.2ba2cab0.js.map
